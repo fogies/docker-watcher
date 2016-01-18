6b329979269ed5c8634a53b4789829096af3374b
James Fogarty <jayfo.github@gmail.com>
2016-01-17 22:52:25 -0800

    Force Recreate

--- buildbot/master/master.py
+++ buildbot/master/master.py
@@ -27,7 +27,7 @@ repositories = [environ for environ in os.environ if environ.startswith('GIT_REP
 
 build_config['change_source'] = []
 for repository_current in repositories:
-    branch = os.environ.get(repository_current + "_BRANCH", 'master')
+    branch = os.environ.get(repository_current + '_BRANCH', 'master')
 
     build_config['change_source'].append(
         buildbot.plugins.changes.GitPoller(
@@ -43,10 +43,10 @@ for repository_current in repositories:
 # 'schedulers' decide how to react to incoming changes
 build_config['schedulers'] = [
     buildbot.plugins.schedulers.SingleBranchScheduler(
-        name="all",
+        name='all',
         change_filter=buildbot.plugins.util.ChangeFilter(),
         # treeStableTimer=None,
-        builderNames=["restart-services"]
+        builderNames=['restart-services']
     )
 ]
 
@@ -66,14 +66,14 @@ for service_current in services:
 for service_current in services:
     factory.addStep(
         buildbot.plugins.steps.ShellCommand(
-            command=['docker-compose', '-f', '/docker-compose/docker-compose.yml', 'up', '--no-deps', '-d', service_current]
+            command=['docker-compose', '-f', '/docker-compose/docker-compose.yml', 'up', '--no-deps', '--force-recreate', '-d', service_current]
         )
     )
 
 build_config['builders'] = [
     buildbot.plugins.util.BuilderConfig(
-        name="restart-services",
-        slavenames=["worker-slave"],
+        name='restart-services',
+        slavenames=['worker-slave'],
         factory=factory
     )
 ]
@@ -89,18 +89,18 @@ build_config['status'] = []
 
 # 'title' will appear at the top of this buildbot installation's html.WebStatus home page
 # (linked to the 'titleURL') and is embedded in the title of the waterfall HTML page.
-build_config['title'] = "Watcher"
-build_config['titleURL'] = "https://github.com/fogies/docker-watcher"
+build_config['title'] = 'Watcher'
+build_config['titleURL'] = 'https://github.com/fogies/docker-watcher'
 
 # 'buildbotURL' string should point to the location where the buildbot's
 # internal web server (usually the html.WebStatus page) is visible.
 # This typically uses the port number set in the Waterfall 'status' entry, but with
 # an externally-visible host name which the buildbot cannot figure out without some help.
-build_config['buildbotURL'] = "http://localhost:8010/"
+build_config['buildbotURL'] = 'http://localhost:8010/'
 
 # DB URL
 build_config['db'] = {
     # This specifies what database buildbot uses to store its state.
     # You can leave this at its default for all but the largest installations.
-    'db_url': "sqlite:///state.sqlite",
+    'db_url': 'sqlite:///state.sqlite',
 }
